fc2f7b48c03e7feceb1a433a5743574bea3e8cb6
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-05-31 05:44:20 -0500

    Commit

--- app/src/main/java/omar/cursos/micalculadora/MainActivity.java
+++ app/src/main/java/omar/cursos/micalculadora/MainActivity.java
@@ -8,7 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Pruebas
         setContentView(R.layout.activity_main);
     }
 }
